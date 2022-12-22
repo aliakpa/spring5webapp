18dfa4b23f20c06c6e171ebd4828d2887043943a
Ali Ak <aliakbartin@gmail.com>
2022-12-22 13:19:40 -0500

    add pojos and repositories

--- src/main/java/guru/springframework/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.repositories;
-
-import guru.springframework.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
